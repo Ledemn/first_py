ebcfac998d5759de999aa474643c1a46783aa708
DESK_PC <serg.li4enko@gmail.com>
2022-05-21 19:45:44 +0300

    Lesson1. Hello.

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print('Hello, World!')
+# print("Хеллоо")
+name = input("введіть своє ім'я: ")
+print("Hello, ", name, "!")    # у виведенні з'являються додатовий пробіл(sep) між змінними
+print("Hello, ", name, "!", sep='')    # 2й варіант запису з сепаратором
+print(f"Hello, {name}!")    # f-рядки
