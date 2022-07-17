32233ae94c44846d40b2483a0b0159113f80ae7f
S-Li <s-li@test.ml>
2022-07-17 22:37:00 +0300

    Book2: Chap2
    Day1

---
+++ book2_chap2.py
@@ -0,0 +1,105 @@
+# Chapter 2: Controlling the Action
+# Making Decisions with if  -----------------------------
+# The result of a simple if when the condition proves true:
+sun = "down"
+if sun == "down":
+    print("Good night!")    # Good night!
+print("I'm here.", '\n')    # I'm here.
+
+
+# Result of simple if when the condition proves false:
+sun = "up"
+if sun == "down":
+    print("Good night!")
+print("I'm here.", '\n')    # I'm here.
+
+
+total = 100
+sales_tax_rate = 0.065
+taxable = True
+if taxable:
+    print(f"Subtotal: ${total:.2f}")
+    sales_tax = total * sales_tax_rate
+    print(f"Sales Tax: ${sales_tax:.2f}")
+    total = total + sales_tax
+print(f"Total: ${total:.2f}", '\n')
+
+
+# if condition:
+#   do indented lines here
+#   ...
+# else:
+#   do indented lines here
+#   ...
+# do remaining un-indented lines no matter what
+
+# Print an initial greeting based on the time of day:
+import datetime as dt
+now = dt.datetime.now()
+if now.hour < 12:
+    print("Good morning")
+else:
+    print("Good afternoon")
+print("I hope you are doing well!", '\n')
+
+
+# Handling multiple else statements with elif.
+light_color = "green"
+if light_color == "green":
+    print("Go")                                     # Go
+elif light_color == "red":
+    print("Stop")
+print("This code executes no matter what", '\n')    # This code executes no matter what
+
+
+# Ternary operations.
+age = 31
+if age < 21:
+    beverage = "milk"
+elif age >= 21 and age < 80:
+    beverage = "beer"
+else:
+    beverage = "prune juice"
+print(f"Have a {beverage.title()}.", '\n')
+
+
+# Repeating a Process with for  -----------------------------
+""" Looping through numbers in a range
+
+for x in range(y):
+   do this
+   do this
+   ...
+un-indented code is executed after the loop"""
+for x in range(1, 11):
+    print(x)
+print("All done.", '\n')
+
+
+# Looping through a string
+"""Using range() in a for loop is optional. You can replace range with a
+string, and the loop repeats once for each character in the string.
+
+for x in string
+    do this
+    do this
+    …
+do this when the loop is done
+"""
+for x in "snorkel":
+    print(x)
+print("Done.", '\n')
+
+
+# Looping through a list
+for x in ["The", "rain", "in", "Spain"]:
+    print(x)
+print("Done", '\n')
+
+
+
+
+
+
+
+# Looping with while  -----------------------------
