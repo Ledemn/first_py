a210e3929d019afe03cab7831a0659723d670d52
S-Li <s-li@test.ml>
2022-07-20 17:35:07 +0300

    Book2: Chap5. Functions.
    new file

---
+++ book2_chap5.py
@@ -0,0 +1,9 @@
+# Book2: Chapter 5. Wrangling Bigger Chunks of Code
+#
+""" IN THIS CHAPTER
+Creating your own function
+Including a comment in a function
+Seeing how to pass information to a function
+Returning values from a function
+Understanding anonymous functions
+"""
